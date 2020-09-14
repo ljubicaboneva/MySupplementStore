fbdb15fc303a7ce643edfe163fd0857a9ee59e82
Љубица Бонева <ljubica.boneva@students.finki.ukim.mk>
2020-09-14 17:46:32 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/emt/mysupplementstore/MysupplementstoreApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.mysupplementstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysupplementstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysupplementstoreApplication.class, args);
+    }
+
+}
---
+++ supplements-catalog/src/main/java/mk/finki/ukim/emt/supplementscatalog/SupplementsCatalogApplication.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.emt.supplementscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplementsCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplementsCatalogApplication.class, args);
+    }
+
+}
